daacbc29240b74356ca731a02e3f19b1acb5a121
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-04-10 21:25:59 +0200

    add: try/exept

--- v.2/main.py
+++ v.2/main.py
@@ -2,8 +2,17 @@ from db import create_tab, add_expense, see_expenses
 
 def main():
 	print('DB EXPENSES RECORD')
-	create_tab()
-	add_expense(50, "prova")
+	try:
+		create_tab()
+	except Exception as e:
+		print(f"[Error] Failed create table: {e}")
+		return
+	try:
+		add_expense(50, "prova")
+	except Exception as e:
+		print(f'[Error] Insert data into table: {e}')
+	
+	
 	see_expenses()
 
 
