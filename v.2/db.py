9b06fc156a45681fee55c6a012dbaceda86e908c
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-04-10 21:06:21 +0200

    fatto

--- v.2/db.py
+++ v.2/db.py
@@ -1,17 +1,28 @@
 import sqlite3
 
 def crea_tabella():
-    conn = sqlite3.connect("database_spese.db")
-    cursor = conn.cursor()
-    
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS spese (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            importo INT,
-            categoria TEXT
-        )
-    """)
-    
-    conn.commit()
-    conn.close()
-    print("Tabella pronta!")
+	conn = sqlite3.connect("database_spese.db")
+	cursor = conn.cursor()
+	
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS spese (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			amount INT,
+			category TEXT
+		)
+	""")
+
+	conn.commit()
+	conn.close()
+	print('Tabella spese Pronta!')
+
+
+def add_expense(amount: int, category: str):
+	conn = sqlite3.connect("database_spese.db")
+	cursor = conn.cursor()
+
+	cursor.execute(f"INSERT INTO spese (amount, category) VALUES (? , ?)", (amount, category))
+	
+	conn.commit
+	conn.close
+	print('Dato Inserito!')
